f8320c8f293906975b0da517e2afbf535cbf2918
hophuocduy2610 <hophuocduy261020@gmail.com>
2022-10-20 00:41:40 +0700

    Hoàn thành tìm kiếm khách hàng có sẵn và hoàn thành load thông tin nhân viên, ngày bán vào trang lập hóa đơn

---
+++ src/main/java/com/example/nhom10_qlhs/controller/LapHoaDonController.java
@@ -0,0 +1,112 @@
+package com.example.nhom10_qlhs.controller;
+
+import com.example.nhom10_qlhs.FxmlLoader;
+import com.example.nhom10_qlhs.GetData;
+import com.example.nhom10_qlhs.connectdb.ConnectDB;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+import java.net.URL;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ResourceBundle;
+
+public class LapHoaDonController implements Initializable {
+
+    @FXML
+    private Button btnTimKH;
+
+    @FXML
+    private TextField txtSDTKH;
+
+    @FXML
+    private Label lblMaKH;
+
+    @FXML
+    private Label lblTenKhachHang;
+
+    @FXML
+    private Label lblNgayBan;
+    @FXML
+    private Label lblMaNV;
+
+    @FXML
+    private Label lblTenNV;
+
+    @FXML
+    private Label lblChucVu;
+
+    public Connection connect;
+    private PreparedStatement prepare;
+    private Statement statement;
+    private ResultSet result;
+    public void timKH(){
+        String sql = "SELECT * FROM KhachHang WHERE sdt = ?";
+        try {
+            connect = ConnectDB.connect();
+            prepare = connect.prepareStatement(sql);
+            prepare.setString(1, txtSDTKH.getText());
+            result = prepare.executeQuery();
+            Alert alert;
+            if (txtSDTKH.getText().isEmpty()) {
+
+                alert = new Alert(Alert.AlertType.ERROR);
+                alert.setTitle("Error");
+                alert.setHeaderText(null);
+                alert.setContentText("Không được bỏ trống");
+                alert.showAndWait();
+            }else {
+                if (result.next()) {
+                    lblMaKH.setText(result.getString("maKH"));
+                    lblTenKhachHang.setText(result.getString("tenKH"));
+                } else {
+                    alert = new Alert(Alert.AlertType.ERROR);
+                    alert.setTitle("Error");
+                    alert.setHeaderText(null);
+                    alert.setContentText("Không tìm thấy khách hàng");
+                    alert.showAndWait();
+                }
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    public void loadThongTinNhanVien(){
+        String sql = "SELECT * FROM NhanVien WHERE maNV = ?";
+        try {
+            System.out.println(GetData.taiKhoan);
+            connect = ConnectDB.connect();
+            prepare = connect.prepareStatement(sql);
+            prepare.setString(1, GetData.taiKhoan);
+            result = prepare.executeQuery();
+            if(result.next()){
+                lblMaNV.setText(result.getString("maNV"));
+                lblTenNV.setText(result.getString("tenNV"));
+                lblChucVu.setText(result.getString("chucVu"));
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+    public void loadNgayBan(){
+        lblNgayBan.setText(LocalDate.now().format(DateTimeFormatter.ofPattern("dd-MM-yyyy")).toString());
+    }
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        loadNgayBan();
+        loadThongTinNhanVien();
+    }
+}
---
+++ src/main/java/com/example/nhom10_qlhs/entities/KhachHang.java
@@ -0,0 +1,86 @@
+package com.example.nhom10_qlhs.entities;
+
+public class KhachHang {
+    private String maKH;
+    private String tenKH;
+    private String diaChi;
+    private String sdt;
+    private String email;
+    private String phai;
+
+    public KhachHang() {
+    }
+
+    public KhachHang(String maKH) {
+        this.maKH = maKH;
+    }
+
+    public KhachHang(String maKH, String tenKH, String diaChi, String sdt, String email, String phai) {
+        this.maKH = maKH;
+        this.tenKH = tenKH;
+        this.diaChi = diaChi;
+        this.sdt = sdt;
+        this.email = email;
+        this.phai = phai;
+    }
+
+    public String getMaKH() {
+        return maKH;
+    }
+
+    public void setMaKH(String maKH) {
+        this.maKH = maKH;
+    }
+
+    public String getTenKH() {
+        return tenKH;
+    }
+
+    public void setTenKH(String tenKH) {
+        this.tenKH = tenKH;
+    }
+
+    public String getDiaChi() {
+        return diaChi;
+    }
+
+    public void setDiaChi(String diaChi) {
+        this.diaChi = diaChi;
+    }
+
+    public String getSdt() {
+        return sdt;
+    }
+
+    public void setSdt(String sdt) {
+        this.sdt = sdt;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhai() {
+        return phai;
+    }
+
+    public void setPhai(String phai) {
+        this.phai = phai;
+    }
+
+    @Override
+    public String toString() {
+        return "KhachHang{" +
+                "maKH='" + maKH + '\'' +
+                ", tenKH='" + tenKH + '\'' +
+                ", diaChi='" + diaChi + '\'' +
+                ", sdt='" + sdt + '\'' +
+                ", email='" + email + '\'' +
+                ", phai='" + phai + '\'' +
+                '}';
+    }
+}
